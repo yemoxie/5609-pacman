b6c2934a2e6826ed4238de9623e1be97d4cbb83f
yemoxie <58982047+yemoxie@users.noreply.github.com>
2022-09-17 14:42:47 +0800

    Update compiler_depend.ts

--- lb2/cmake-build-debug/CMakeFiles/CP5609-Lab2.dir/compiler_depend.ts
+++ lb2/cmake-build-debug/CMakeFiles/CP5609-Lab2.dir/compiler_depend.ts
@@ -1,2 +1,2 @@
 # CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for CP5609-Lab2.
+# Timestamp file for compiler generated dependencies management for lb2.
