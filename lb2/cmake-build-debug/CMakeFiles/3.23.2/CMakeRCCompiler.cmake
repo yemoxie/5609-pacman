0fb51ac7dcd5112de3fdb705d05489a84745d3c3
yemoxie <58982047+yemoxie@users.noreply.github.com>
2022-09-17 14:37:13 +0800

    Update CMakeRCCompiler.cmake

--- lb2/cmake-build-debug/CMakeFiles/3.23.2/CMakeRCCompiler.cmake
+++ lb2/cmake-build-debug/CMakeFiles/3.23.2/CMakeRCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_RC_COMPILER "C:/Program Files/MinGW/x86_64-8.1.0-release-posix-sjlj-rt_v6-rev0/mingw64/bin/windres.exe")
+set(CMAKE_RC_COMPILER "C:/Program Files/MinGW/x86_64-8.1.0-release-posix-sjlj-rt_v6-rev0/scath64/bin/windres.exe")
 set(CMAKE_RC_COMPILER_ARG1 "")
 set(CMAKE_RC_COMPILER_LOADED 1)
 set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
