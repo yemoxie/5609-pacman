c5c056aade642e2ecd9e5e8cca759c90ff16f672
yemoxie <58982047+yemoxie@users.noreply.github.com>
2022-09-17 14:38:53 +0800

    Update CMakeCXXCompilerId.cpp

--- lb2/cmake-build-debug/CMakeFiles/3.23.2/CompilerIdCXX/CMakeCXXCompilerId.cpp
+++ lb2/cmake-build-debug/CMakeFiles/3.23.2/CompilerIdCXX/CMakeCXXCompilerId.cpp
@@ -1,5 +1,5 @@
 /* This source file must have a .cpp extension so that all C++ compilers
-   recognize the extension without flags.  Borland does not know .cxx for
+   recognize the extension without flags.  Borland does not know .cxx for 
    example.  */
 #ifndef __cplusplus
 # error "A C compiler has been selected for C++."
